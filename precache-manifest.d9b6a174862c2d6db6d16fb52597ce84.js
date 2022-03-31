0f10e187fbb643e5743d02079220dd7c43ca8776
Franco Beratz <francoberatz.fb@gmail.com>
2022-03-31 15:11:38 -0300

    Updates

---
+++ precache-manifest.d9b6a174862c2d6db6d16fb52597ce84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3eb45e27f4cc1a00cc90ee335b2f030",
+    "url": "/FranckoBz94/movie/index.html"
+  },
+  {
+    "revision": "83b08264fc001da87d58",
+    "url": "/FranckoBz94/movie/static/css/2.0376a2dc.chunk.css"
+  },
+  {
+    "revision": "6bcf9349245b9e182c0e",
+    "url": "/FranckoBz94/movie/static/css/main.16140cf8.chunk.css"
+  },
+  {
+    "revision": "83b08264fc001da87d58",
+    "url": "/FranckoBz94/movie/static/js/2.5281ddec.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/FranckoBz94/movie/static/js/2.5281ddec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcf9349245b9e182c0e",
+    "url": "/FranckoBz94/movie/static/js/main.7cd4ce20.chunk.js"
+  },
+  {
+    "revision": "bde2b6cd4b1a9b9754b6",
+    "url": "/FranckoBz94/movie/static/js/runtime-main.39341f3c.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/FranckoBz94/movie/static/media/logo.cb90c5bd.svg"
+  }
+]);
